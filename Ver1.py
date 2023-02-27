fc5e848a24c2856651b72ae1447445ed248b974d
WhiteHodok <39564937+WhiteHodok@users.noreply.github.com>
2023-02-27 16:00:03 +0600

    Add files via upload

--- Ver1.py
+++ Ver1.py
@@ -2,16 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import colorama
 
 customers = pd.read_csv('customers.csv') # Считываем дата-сет
 
 #print(customers.head(3)) я самостоятельно просмотрел тип данных в этом сете и решил закоммитить строчку
 # Часть с циферками
-print("Краткое info о нашем дата-сете: \n")
-print(customers.info())
-print("")
-print(customers.describe())
+#print("Краткое info о нашем дата-сете: \n")
+#print(customers.info())
+#print("")
+#print(customers.describe())
+#print('Средний возраст покупателей :',customers['Age'].mean())
+#print('Среднее количество детей у покупателей',customers['Family Size'].mean())
+#print('Самая часто-встречаемая профессия у покупателей:', customers['Profession'].mode())
+
 #Часть с рисуночками
 #sns.countplot(data=customers, x='Gender') # проанализируем покупателей по полу
 #plt.show() # Если работать не в колабе и не в юпитере ,то для людей не знакомых с пайтоном будет проблематичено вызвать график и вообще понять почему matplot и seaborn связаны
@@ -27,12 +33,17 @@ print(customers.describe())
 #sns.heatmap(data=customers.corr(), annot=True)
 #plt.show()
 #Возраст и очки
-#sns.scatterplot(data=customers,x='Age',y='Spending Score (1-100)')
 #plt.savefig('scatter.png', dpi=300, bbox_inches='tight') итог работы этой фуфнкции в картинках :)
-#plt.show()
 #Корреляция профессии и зарплаты
 #grid = sns.FacetGrid(data=customers,col = 'Profession', hue='Profession', col_wrap=3)
 #grid.map(sns.scatterplot, "Profession", "Annual Income ($)")
+#plt.show()
+# Опыт работы и возраст
+#sns.residplot(data=customers, x='Age', y='Work Experience',lowess=True,line_kws=dict(color="r"))
+#plt.show()
+# Возраст и потраченные очки
+#sns.scatterplot(data=customers, x="Age", y="Spending Score (1-100)")
+#sns.rugplot(data=customers, x="Age", y="Spending Score (1-100)")
 #plt.show()
 
 
